bfb994b77808ca3acad89134487aad92597ef7fb
Convidado <luisfernandorsalves@gmail.com>
2017-10-24 20:53:32 -0200

    carregando tudo na memoria

--- trabalho.c
+++ trabalho.c
@@ -97,7 +97,7 @@ void treino(struct Musica caso_teste,int qdeElem){
 	}
 
 }
-void leituraArquivo(char dir[],struct Musica **musicas_return,int *qdeElem){
+void leituraArquivo(char dir[],struct Musica **musicas_return,int *qdeElem,int *qdeMusicas){
 	size_t len = 0;
 	FILE *file;
 	char *line = NULL,
@@ -106,8 +106,8 @@ void leituraArquivo(char dir[],struct Musica **musicas_return,int *qdeElem){
 	*qdeElem=0;
 	float *array;
 	file = fopen(dir, "r");
-	int ind_array=0,
-	    qdeMusicas=0;
+	int ind_array=0;
+	*qdeMusicas=0;
 
 	struct Musica *musicas = (struct Musica *)malloc(sizeof(struct Musica));
 	
@@ -119,19 +119,19 @@ void leituraArquivo(char dir[],struct Musica **musicas_return,int *qdeElem){
     			*qdeElem = countChars(line,',');//retorna 59 em Split[58]: 0.283174, Split[59]: forro
     		}
     		//debugArq(line,qdeElem);
-    		musicas = (struct Musica *)realloc(musicas,(qdeMusicas+1)*sizeof(Musica));
-    		musicas[qdeMusicas].array = (float *)malloc(*qdeElem*sizeof(float));
+    		musicas = (struct Musica *)realloc(musicas,(*qdeMusicas+1)*sizeof(Musica));
+    		musicas[*qdeMusicas].array = (float *)malloc(*qdeElem*sizeof(float));
 
     		while((split = strsep(&line,",")) != NULL ){
     			if(ind_array!=*qdeElem){
-	    			musicas[qdeMusicas].array[ind_array] = strtof(split, NULL);
+	    			musicas[*qdeMusicas].array[ind_array] = strtof(split, NULL);
 	       		}else{
-	       			musicas[qdeMusicas].genero = strtok(split, "\n");
+	       			musicas[*qdeMusicas].genero = strtok(split, "\n");
 	       		}
        			ind_array++;
     		}    	
     		//treino(musicas[qdeMusicas],qdeElem);
-    		qdeMusicas++;
+    		*qdeMusicas+=1;
 		}
 		*musicas_return = musicas;
 
@@ -141,25 +141,55 @@ void leituraArquivo(char dir[],struct Musica **musicas_return,int *qdeElem){
 	}
     
 }
+void calcula_menor_distancia(Musica caso_teste, Musica *casos_treino,int qdeElem,int size_treino,float *MENOR_DISTANCIA, Musica *MELHOR_MUSICA){
+	*MENOR_DISTANCIA=FLT_MAX;
+	float distancia;
+	for(int i=0;i<size_treino;i++){
+		distancia = distanciaMusica(caso_teste,casos_treino[i],qdeElem);
+		/*printf("\n distancia %.10f ",distancia);
+		printf("\n gen %s ",caso_teste.genero);
+		printf("\n gen %s ",casos_treino[i].genero);
+		/**/
+		if(distancia < *MENOR_DISTANCIA){
+			*MENOR_DISTANCIA = distancia;
+			*MELHOR_MUSICA = casos_treino[i];
+		}
+	}
+}
 int main(int argc, char *argv[]){		
 	char *line = NULL,
 	     *split,
 		 read,
-		 dir_test[]="bases/test_59.data",
-		 dir_treino[]="bases/train59.data";
+		 dir_test[]="bases/test_10.data",
+		 dir_treino[]="bases/train_10.data";
 
 	int qdeElem=0,
 	    ind_array=0,
-	    qdeMusicas=0;
+	    qdeMusicas=0,
+	    size_teste,
+	    size_treino;
 	size_t len = 0;
+	float MENOR_DISTANCIA;
+	struct Musica MELHOR_MUSICA;
 	struct Musica *casos_teste = (struct Musica *)malloc(sizeof(struct Musica));
 	struct Musica *casos_treino = (struct Musica *)malloc(sizeof(struct Musica));
 	/*-----var def-----*/
 	printf("\n::Iniciando::");
-	leituraArquivo(dir_test,&casos_teste,&qdeElem);
-	leituraArquivo(dir_test,&casos_treino,&qdeElem);
+
+	leituraArquivo(dir_test,&casos_teste,&qdeElem,&size_teste);
+	leituraArquivo(dir_treino,&casos_treino,&qdeElem,&size_treino);
+
+	/*printf("\nSize [%d]",size_teste);
+	printf("\nSize [%d]",size_treino);
+*/
+	for(int i=0;i<size_teste;i++){
+		calcula_menor_distancia(casos_teste[i],casos_treino,qdeElem,size_treino,&MENOR_DISTANCIA,&MELHOR_MUSICA);
+		printf("\n Genero: [%s => %s]; Menor distancia %.10f ",casos_teste[i].genero,MELHOR_MUSICA.genero,MENOR_DISTANCIA);
+	}
+	/*
 	print_array(casos_teste[0].array,qdeElem);
 	print_array(casos_treino[0].array,qdeElem);
+	*/
 	printf("\n");
     return 0;
 }
